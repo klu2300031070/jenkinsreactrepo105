9078ced1668641b88152675da701c471bfa165be
Kamesh <2300031070@kluniversity.in>
2025-08-04 14:32:21 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Dashboard from "./pages/Dashboard";
+
+import Login from "./pages/Login";
+import DepartmentPage from "./pages/DepartmentPage";
+import EmployeeForm from "./pages/EmployeeFrom";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/employee-form" element={<EmployeeForm />} />
+        <Route path="/departments" element={<DepartmentPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
+
+
+export default App;
